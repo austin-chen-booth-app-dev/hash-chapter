20c39da43a3e1203a5d51e3bd6247ff61947cc68
austin-chen-booth <achen52@chicagobooth.edu>
2023-01-18 18:21:58 +0000

    Homework

--- hash_find_value.rb
+++ hash_find_value.rb
@@ -4,6 +4,18 @@
 #   If you find the number, print out "100 is under the key: a."
 #   If you don't find the number print out "Could not find the integer 100"
 
-sample_hash = {:a => 100, :b => 200, :c => 300, :d => 400, :e => rand(200), :f => 600, :g => 0 }
+sample_hash = { :a => 100, :b => 200, :c => 300, :d => 400, :e => rand(200), :f => 600, :g => 0 }
 
 p "Enter an integer to find:"
+
+integer_input = gets.chomp.to_i
+
+if not sample_hash.has_value?(integer_input)
+  p("Could not find the integer #{integer_input}")
+else
+  sample_hash.each do |key, value|
+    if value == integer_input
+      p("#{integer_input} is under the key: #{key}")
+    end
+  end
+end
--- hash_person.rb
+++ hash_person.rb
@@ -2,3 +2,13 @@
 # Make sure the value of the :age key is an Integer
 
 p "Enter a name, age, and occupation separated by spaces:"
+
+profile_input = gets.chomp.split
+
+profile_hash = Hash.new
+
+profile_hash.store(:name, profile_input[0])
+profile_hash.store(:age, profile_input[1].to_i)
+profile_hash.store(:occupation, profile_input[2])
+
+p(profile_hash)
